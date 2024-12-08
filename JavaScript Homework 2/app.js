62d3196f2407498fdc55550c386d209c8abd0aec
Flamuravd <145875910+Flamuravd@users.noreply.github.com>
2024-12-08 16:02:42 +0100

    Homework 2

---
+++ JavaScript Homework 2/app.js
@@ -0,0 +1,29 @@
+let chineseZodiac = 2024;
+
+let resultOfZodiac = (chineseZodiac - 4) % 12;
+
+if(resultOfZodiac === 0) {
+  console.log("Your Chinese Zodiac year is Rat");
+} else if(resultOfZodiac === 1) {
+  console.log("Your Chinese Zodiac year is Ox");
+} else if(resultOfZodiac === 2) {
+  console.log("Your Chinese Zodiac year is Tiger");
+} else if(resultOfZodiac === 3) {
+  console.log("Your Chinese Zodiac year is Rabbit");
+} else if(resultOfZodiac === 4) {
+  console.log("Your Chinese Zodiac year is Dragon");
+} else if(resultOfZodiac === 5) {
+  console.log("Your Chinese Zodiac year is Snake");
+} else if(resultOfZodiac === 6) {
+  console.log("Your Chinese Zodiac year is Horse");
+} else if(resultOfZodiac === 7) {
+  console.log("Your Chinese Zodiac year is Goat");
+} else if(resultOfZodiac === 8) {
+  console.log("Your Chinese Zodiac year is Monkey");
+} else if(resultOfZodiac === 9) {
+  console.log("Your Chinese Zodiac year is Rooster");
+} else if(resultOfZodiac === 10) {
+  console.log("Your Chinese Zodiac year is Dog");
+} else if(resultOfZodiac === 11) {
+  console.log("Your Chinese Zodiac year is Pig");
+}
