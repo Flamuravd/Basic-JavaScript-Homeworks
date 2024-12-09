852c2cfa7ea172223333e4881a60a8494d84278d
Flamuravd <145875910+Flamuravd@users.noreply.github.com>
2024-12-09 17:18:18 +0100

    Homework 1

--- JavaScript Homework 1/app.js
+++ JavaScript Homework 1/app.js
@@ -1,8 +1,9 @@
 let numOfPhones = 30;
 let priceOfPhone = 119.95;
+let taxRate = 1.05;
 
 // Before 5% tax
 console.log(`The cost of the phone without taxes is $${numOfPhones * priceOfPhone}`); // Output 3598.5
 
 // After 5% tax
-console.log(`The cost of the phone with 5% tax is $${numOfPhones * priceOfPhone * 1.05}`); // Output 3778.425
+console.log(`The cost of the phone with 5% tax is $${numOfPhones * priceOfPhone * taxRate}`); // Output 3778.425
